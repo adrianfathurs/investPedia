be76ec9c196c4c25f4c53906430d0a689d54e3d1
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-05-04 22:23:25 +0700

    refactor(user): using function has been created

--- handler/user.go
+++ handler/user.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"investPedia/helper"
 	"investPedia/user"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type userHandler struct {
@@ -20,12 +22,25 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		var errors []string
+		for _, e := range err.(validator.ValidationErrors) {
+			errors = append(errors, e.Error())
+		}
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse("Input is not valid", "failed", http.StatusUnprocessableEntity, errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
 	}
-	user, err := h.userService.RegisterUser(input)
+	newUser, err := h.userService.RegisterUser(input)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.APIResponse("Register account tfailed", "failed", http.StatusBadRequest, nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
-	c.JSON(http.StatusOK, user)
+
+	formatter := user.FormatterUserResponse(newUser, "")
+
+	response := helper.APIResponse("Account has been registered", "success", http.StatusOK, formatter)
+	c.JSON(http.StatusOK, response)
 }
