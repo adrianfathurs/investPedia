d9678b41692d827d0962dbd5c11e2e6cecac5288
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-05-04 22:22:47 +0700

    refactor(input): add some validator on struct using gin

--- user/input.go
+++ user/input.go
@@ -1,9 +1,9 @@
 package user
 
 type RegisterUserInput struct {
-	FullName   string
-	Occupation string
-	Password   string
+	FullName   string `json:"fullname" binding:"required"`
+	Occupation string `json:"occupation" binding:"required"`
+	Password   string `json:"password" binding:"required"`
 	Role       string
-	Email      string
+	Email      string `json:"email" binding:"required,email"`
 }
