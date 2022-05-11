ee34631f216dc7cb0fa70f2e6a7f047d5838ea47
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-05-12 04:52:47 +0700

    feat(repository): add FindCampaignByID to get campaign data detail

--- campaign/repository.go
+++ campaign/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	FindAllCampaign() ([]Campaign, error)
 	FindCampaignByUserID(userID int) ([]Campaign, error)
+	FindCampaignByID(ID int) (Campaign, error)
 }
 
 type repository struct {
@@ -24,6 +25,7 @@ func (r *repository) FindAllCampaign() ([]Campaign, error) {
 	return campaigns, nil
 
 }
+
 func (r *repository) FindCampaignByUserID(userID int) ([]Campaign, error) {
 	var campaigns []Campaign
 	err := r.db.Where("user_id = ?", userID).Preload("CampaignImages", "campaign_images.isPrimary = 1").Find(&campaigns).Error
@@ -31,4 +33,13 @@ func (r *repository) FindCampaignByUserID(userID int) ([]Campaign, error) {
 		return campaigns, err
 	}
 	return campaigns, nil
+}
+
+func (r *repository) FindCampaignByID(ID int) (Campaign, error) {
+	var campaign Campaign
+	err := r.db.Where("id =?", ID).Preload("User").Preload("CampaignImages").Find(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+	return campaign, nil
 }
