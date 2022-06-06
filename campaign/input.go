08028631ad095edba3d44acc07ae1a68fd86e9c6
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-06-07 05:40:01 +0700

    feat(input): add createCampaign Input

--- campaign/input.go
+++ campaign/input.go
@@ -1,5 +1,16 @@
 package campaign
 
+import "investPedia/user"
+
 type CampaignInput struct {
 	ID int `uri:"id" binding:"required`
+}
+
+type CreateCampaignInput struct {
+	Title        string `json:"title" binding:"required"`
+	SubTitle     string `json:"sub_title" binding:"required"`
+	Desctiption  string `json:"description" binding:"required"`
+	TargetInvest int    `json:"target_invest" binding:"required"`
+	Perks        string `json;"perks" binding:"required"`
+	User         user.User
 }
