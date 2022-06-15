e2f8988269f2da5dbc5b95d94c4da9945adbf8c9
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-06-15 14:01:36 +0700

    feat(formatter): add func to set format response for client on transaction

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -1,1 +1,35 @@
 package transaction
+
+import (
+	"time"
+)
+
+type CampaignTransactionFormatter struct {
+	ID        int       `json:"id"`
+	FullName  string    `json:"full_name"`
+	Amount    int       `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func FormatCampaignTransaction(campaignTransaction Transaction) CampaignTransactionFormatter {
+	formatter := CampaignTransactionFormatter{
+		ID:        campaignTransaction.ID,
+		FullName:  campaignTransaction.User.FullName,
+		Amount:    campaignTransaction.Amount,
+		CreatedAt: campaignTransaction.CreatedAt,
+	}
+	return formatter
+}
+
+func FormatListCampaignTransaction(campaignTransaction []Transaction) []CampaignTransactionFormatter {
+	if len(campaignTransaction) == 0 {
+		return []CampaignTransactionFormatter{}
+	}
+
+	var formatter []CampaignTransactionFormatter
+
+	for _, item := range campaignTransaction {
+		formatter = append(formatter, FormatCampaignTransaction(item))
+	}
+	return formatter
+}
