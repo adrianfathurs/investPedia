f2197f869260f9392a112b64fc18482045866ccd
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-07-05 01:57:17 +0700

    feat(formatter): add formatter struct for response transaction

--- transaction/formatter.go
+++ transaction/formatter.go
@@ -11,6 +11,18 @@ type CampaignTransactionFormatter struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+type CampaignTitleandImages struct {
+	Title    string `json:"name"`
+	FileName string `json:"image_url"`
+}
+type UserTransactionsFormatter struct {
+	ID                int                    `json:"id"`
+	Amount            int                    `json:"amount"`
+	TransactionStatus string                 `json:"status"`
+	CreatedAt         time.Time              `json:"created_at"`
+	Campaign          CampaignTitleandImages `json:"campaign"`
+}
+
 func FormatCampaignTransaction(campaignTransaction Transaction) CampaignTransactionFormatter {
 	formatter := CampaignTransactionFormatter{
 		ID:        campaignTransaction.ID,
@@ -30,6 +42,36 @@ func FormatListCampaignTransaction(campaignTransaction []Transaction) []Campaign
 
 	for _, item := range campaignTransaction {
 		formatter = append(formatter, FormatCampaignTransaction(item))
+	}
+	return formatter
+}
+
+func FormatListUserTransaction(transactions []Transaction) []UserTransactionsFormatter {
+	if len(transactions) == 0 {
+		return []UserTransactionsFormatter{}
+	}
+
+	var formatter []UserTransactionsFormatter
+
+	for _, item := range transactions {
+		formatter = append(formatter, FormatUserTransaction(item))
+	}
+	return formatter
+}
+
+func FormatUserTransaction(transaction Transaction) UserTransactionsFormatter {
+	campaign := CampaignTitleandImages{}
+	campaign.Title = transaction.Campaign.Title
+	if len(transaction.Campaign.CampaignImages) > 0 {
+		campaign.FileName = transaction.Campaign.CampaignImages[0].FileName
+	}
+
+	formatter := UserTransactionsFormatter{
+		ID:                transaction.ID,
+		Amount:            transaction.Amount,
+		TransactionStatus: transaction.TransactionStatus,
+		CreatedAt:         transaction.CreatedAt,
+		Campaign:          campaign,
 	}
 	return formatter
 }
