7310608aae95e305e1535321af3014bd747a845c
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-07-05 01:57:55 +0700

    feat(repository): add getTransactionByUserID repository

--- transaction/repository.go
+++ transaction/repository.go
@@ -6,6 +6,7 @@ import (
 
 type Repository interface {
 	GetTransactionsByCampaignID(campaignID int) ([]Transaction, error)
+	GetTransactionsByUserID(userID int) ([]Transaction, error)
 }
 type repository struct {
 	db *gorm.DB
@@ -18,6 +19,15 @@ func NewRepository(db *gorm.DB) *repository {
 func (r *repository) GetTransactionsByCampaignID(campaignID int) ([]Transaction, error) {
 	var transactions []Transaction
 	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Order("created_at desc").Find(&transactions).Error
+	if err != nil {
+		return transactions, err
+	}
+	return transactions, nil
+}
+
+func (r *repository) GetTransactionsByUserID(userID int) ([]Transaction, error) {
+	var transactions []Transaction
+	err := r.db.Preload("Campaign.CampaignImages", "campaign_images.is_primary = 1").Where("user_id = ?", userID).Order("created_at desc").Find(&transactions).Error
 	if err != nil {
 		return transactions, err
 	}
