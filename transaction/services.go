87ccf99410ec346157f812beb4db919c62e634f1
Adrian Fathur Setyawan <fatursetiawan80@gmail.com>
2022-09-07 17:02:35 +0700

    feat(services): add some code on createTransactionInput on package

--- transaction/services.go
+++ transaction/services.go
@@ -3,19 +3,22 @@ package transaction
 import (
 	"errors"
 	"investPedia/campaign"
+	"investPedia/payment"
 )
 
 type Service interface {
 	GetTransactionsByCampaignID(campaignTransaction CampaignTransactionInput) ([]Transaction, error)
 	GetTransactionsByUserID(userID int) ([]Transaction, error)
+	CreateTransactionInput(input CreateTransactionInput) (Transaction, error)
 }
 type service struct {
 	transactionRepository Repository
 	campaignRepository    campaign.Repository
+	paymentService        payment.Services
 }
 
-func NewService(transactionRepository Repository, campaignRepository campaign.Repository) *service {
-	return &service{transactionRepository, campaignRepository}
+func NewService(transactionRepository Repository, campaignRepository campaign.Repository, paymentService payment.Services) *service {
+	return &service{transactionRepository, campaignRepository, paymentService}
 }
 
 func (s *service) GetTransactionsByCampaignID(campaignTransaction CampaignTransactionInput) ([]Transaction, error) {
@@ -42,4 +45,34 @@ func (s *service) GetTransactionsByUserID(userID int) ([]Transaction, error) {
 		return transaction, err
 	}
 	return transaction, nil
+}
+
+func (s *service) CreateTransactionInput(input CreateTransactionInput) (Transaction, error) {
+	trans := Transaction{}
+	trans.Amount = input.Amount
+	trans.CampaignId = input.CampaignID
+	trans.UserId = input.User.ID
+	trans.TransactionStatus = "pending"
+	trans.Code = ""
+	// create data transaction and column payment_url still null
+	newTransaction, err := s.transactionRepository.Save(trans)
+	if err != nil {
+		return newTransaction, err
+	}
+	paymentTransaction := payment.Transaction{
+		ID:     newTransaction.ID,
+		Amount: newTransaction.Amount,
+	}
+	// we can get payment url from midtrans generate
+	paymentUrl, err := s.paymentService.GetPaymentURL(paymentTransaction, input.User)
+	if err != nil {
+		return newTransaction, err
+	}
+	// we data on transaction table and update column payment_url
+	newTransaction.PaymentURL = paymentUrl
+	newTransaction, err = s.transactionRepository.Update(newTransaction)
+	if err != nil {
+		return newTransaction, err
+	}
+	return newTransaction, nil
 }
